3cf635fcc748bb3dc6735e1cd346e2661fa02174
Jeroen van Hattem <jeroenvanhattem@outlook.com>
2018-05-14 11:57:07 +0200

    Changed some minor things

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
     int coordinates[3] = {4, 12, 5};
+    int speed = 5000;
 
     hwlib::wait_ms(1000);
 
     RobotArm uarm_swift_pro(hwlib::target::pin_out(hwlib::target::pins::d7));
 
-    uarm_swift_pro.move(coordinates, 10);
+    uarm_swift_pro.move(coordinates, speed);
 
     return 0;
 }
