bb6172e428f03e8936fd68c1b630875b98243711
Jeroen van Hattem <jeroenvanhattem@outlook.com>
2018-05-14 09:57:44 +0200

    Wrote Doxygen for settings.cpp

--- src/settings.cpp
+++ src/settings.cpp
@@ -2,11 +2,17 @@
 settings::settings(int motorSpeed) : motorSpeed(motorSpeed) {
 }
 
+/// setMotorSpeed()
+//
+/// This function sets a fixed motorspeed for moving the arm.
 void settings::setMotorSpeed(int speed) {
     motorSpeed = speed;
     hwlib::cout << "Set motorspeed to: " << motorSpeed << hwlib::endl;
 }
 
+/// returnMotorSpeed()
+//
+/// This function returns the motor speed
 int settings::returnMotorSpeed() {
     return motorSpeed;
 }
