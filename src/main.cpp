452e8d1194454c18dddda1e5a5ad45d9205867b9
Jeroen <jeroenvanhattem@outlook.com>
2018-05-22 11:45:53 +0200

    Cleaned up the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,19 @@
 /**
  * @file
  * @brief     Main function
- * @author    Jeroen van Hattem
+ * @author    Jeroen van Hattem and Jeffrey de Waal
  * @license   MIT
  */
+
 #include "robot_arm.hpp"
 #include "settings.hpp"
 #include "wrap-hwlib.hpp"
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
-    int coordinates[3] = {4, 12, 5};
-    int speed = 5000;
-
     hwlib::wait_ms(1000);
 
     RobotArm uarmSwiftPro;
-    // uarmSwiftPro.move(coordinates, speed);
-    // hwlib::cout << uarmSwiftPro.getCoordinates('x') << hwlib::endl;
-    // hwlib::cout << uarmSwiftPro.getCoordinates('y') << hwlib::endl;
-    // hwlib::cout << uarmSwiftPro.getCoordinates('z') << hwlib::endl;
 
     return 0;
 }
