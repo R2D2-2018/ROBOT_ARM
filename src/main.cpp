527036cb283b162f543017ebe59c82af2452ebca
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-14 13:04:42 +0200

    Nicer runtime test

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,16 @@ int main() {
 
     RobotArm uarmSwiftPro;
 
+    if (!uarmSwiftPro.isConnected()) {
+        hwlib::cout << "Please connect the uArm Swift Pro" << hwlib::endl;
+
+        while (!uarmSwiftPro.isConnected()) hwlib::wait_ms(200);
+    }
+
+    hwlib::cout << "uArm Swift Pro is connected!" << hwlib::endl;
+
+    hwlib::wait_ms(2000);
+
     while (true) {
         uarmSwiftPro.move(Coordinate3D(200, 150, 100), 5000);
         uarmSwiftPro.executeAction("M2231 V1\n");
