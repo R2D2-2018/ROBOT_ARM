d613de2f829aa611855c338cfc1dcc1d0e9804ce
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-14 13:03:36 +0200

    Updated build environment

--- Sources.cmake
+++ Sources.cmake
@@ -15,9 +15,6 @@ link_libraries (gcc)
 
 set (hwlib ${build_environment}/libraries/hwlib)
 include_directories (${hwlib}/library)
-add_definitions (-DBMPTK_TARGET_arduino_due
-                 -DBMPTK_TARGET=arduino_due
-                 -DBMPTK_BAUDRATE=19200)
 
 set (catch ${build_environment}/libraries/Catch2)
 include_directories (${catch}/single_include)
