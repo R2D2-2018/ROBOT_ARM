cfac46366963fc832cb6b63bd5356547a2d7a2b9
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-18 13:07:17 +0200

    Made Coordinate3D a struct instead of a class

--- src/coordinate3d.hpp
+++ src/coordinate3d.hpp
@@ -1,13 +1,13 @@
 /**
  * @file
- * @brief     This class contains the Coordinate3D Class
- * @author    Jeroen van Hattem and Jeffrey de Waal
+ * @brief     This class contains the Coordinate3D struct
+ * @author    Jeroen van Hattem, Jeffrey de Waal and Wiebe van Breukelen
  * @license   See LICENSE
  */
 #ifndef COORDINATE3D_HPP
 #define COORDINATE3D_HPP
 
-class Coordinate3D {
+struct Coordinate3D {
   public:
     int x;
     int y;
