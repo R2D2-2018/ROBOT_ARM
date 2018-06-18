2891524ca568724380fdd6944ebee8d35f3d11f3
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-18 13:34:17 +0200

    Written math operators for Coordinate3D

--- src/coordinate3d.hpp
+++ src/coordinate3d.hpp
@@ -1,13 +1,13 @@
 /**
  * @file
- * @brief     This class contains the Coordinate3D Class
- * @author    Jeroen van Hattem and Jeffrey de Waal
+ * @brief     This class contains the Coordinate3D struct
+ * @author    Jeroen van Hattem, Jeffrey de Waal, Wiebe van Breukelen
  * @license   See LICENSE
  */
 #ifndef COORDINATE3D_HPP
 #define COORDINATE3D_HPP
 
-class Coordinate3D {
+struct Coordinate3D {
   public:
     int x;
     int y;
@@ -20,17 +20,47 @@ class Coordinate3D {
      *
      */
     Coordinate3D();
+
     /**
      * @brief Construct a new Coordinate3D object
      *
      * The default constructor to create a new Coordinate3D object.
-     *
-     * @param x : int
-     * @param y : int
-     * @param z : int
-     *
+     * 
+     * @param x X constant
+     * @param y Y constant
+     * @param z Z constant
      */
-    Coordinate3D(int x, int y, int z);
+    Coordinate3D(const int x, const int y, const int z);
+
+    /**
+     * @brief Used to add coordinate.
+     * 
+     * @param coord Coordinate.
+     */
+    void operator+=(const Coordinate3D &coord);
+
+    /**
+     * @brief Used to substract coordinate.
+     * 
+     * @param coord Coordinate.
+     */
+    void operator-=(const Coordinate3D &coord);
+
+    /**
+     * @brief Operator+
+     * 
+     * @param coord 
+     * @return Coordinate3D 
+     */
+    Coordinate3D operator+(const Coordinate3D &coord) const;
+
+    /**
+     * @brief 
+     * 
+     * @param coord 
+     * @return Coordinate3D 
+     */
+    Coordinate3D operator-(const Coordinate3D &coord) const;
 };
 
 #endif // COORDINATE3D_HPP
