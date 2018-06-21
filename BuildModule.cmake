4d45b4e349cfd1e5e9a1c7b564503837708e9175
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-21 12:42:59 +0200

    Using universal UART library within the build environment

--- BuildModule.cmake
+++ BuildModule.cmake
@@ -1,10 +1,13 @@
 include (${build_environment}/flags.cmake)
 
+set (uart_lib ${build_environment}/libraries/UART_LIB)
+include_directories (${uart_lib}/src)
+
 set (sources ${sources}
     src/main.cpp
     src/wrap-hwlib.cpp
     src/libc-stub.cpp
-    src/hardware_uart.cpp
+    ${uart_lib}/src/hardware_uart.cpp
 )
 
 add_definitions (-DBMPTK_TARGET_arduino_due
--- Sources.cmake
+++ Sources.cmake
@@ -8,13 +8,16 @@ include_directories (${hwlib}/library)
 set (catch ${build_environment}/libraries/Catch2)
 include_directories (${catch}/single_include)
 
+set (uart_lib ${build_environment}/libraries/UART_LIB)
+include_directories (${uart_lib}/src)
+
 # Source Files:
 
 set (sources
     src/coordinate3d.cpp
     src/libc-stub.cpp
-    src/main.cpp
     src/robot_arm.cpp
-	src/uart_connection.cpp
+    ${uart_lib}/src/mock_uart.cpp
+    ${uart_lib}/src/uart_connection.cpp
     src/wrap-hwlib.cpp
 )
