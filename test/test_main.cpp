8ebfd5000541d6c84c4a46d223dabc1fc4baac52
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-22 11:49:49 +0200

    Written more tests

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,76 +1,50 @@
 /**
  * @file
  * @brief     Test file
- * @author    Jeroen van Hattem
+ * @author    Jeroen van Hattem, Wiebe van Breukelen
  * @license   MIT
  */
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
-#include "../src/coordinate3d.hpp"
+
 #include "../src/robot_arm.hpp"
 #include "mock_uart.hpp"
 
 #include "catch.hpp"
+#include "test_coordinate3d.hpp"
+#include "test_move_queue.hpp"
 
 UARTLib::MockUART comm(115200);
 hwlib::test::pin_in<8> testPinIn{1, 0, 1, 0, 1, 0, 1, 1};
 RobotArm::RobotArm uarmSwiftPro(comm, testPinIn);
-char text[100];
+
 
 TEST_CASE("Append char* with another char*") {
+    char text[100] = "Hello, ";
     uarmSwiftPro.stradd(text, "how are you?");
 
-    //REQUIRE(text == "Hi, how are you?");
+    REQUIRE_THAT(text, Catch::Matchers::Equals("Hello, how are you?"));
 }
 
 TEST_CASE("Copy char* to another char *") {
-    uarmSwiftPro.strcopy(text, "Hi, ");
+    char text[100] = "Hello World!";
+    uarmSwiftPro.strcopy(text, "Hi");
 
-    //REQUIRE(text == "Hi, ");
+    REQUIRE_THAT(text, Catch::Matchers::Equals("Hi"));
 }
 
 TEST_CASE("Convert integer to char *") {
     int x = 100;
-    char buffer[5];
+    char buffer[4];
 
     uarmSwiftPro.intToChar(x, buffer);
 
-    //REQUIRE(buffer == "100");
+    REQUIRE_THAT(buffer, Catch::Matchers::Equals("100"));
 }
 
-TEST_CASE("Coordinates 3D get coordinates positive") {
-    RobotArm::Coordinate3D coordinates(140, 150, 130);
+TEST_CASE("Convert char * to integer") {
+    char buffer[4] = "100";
 
-    REQUIRE(coordinates.x == 140);
-    REQUIRE(coordinates.y == 150);
-    REQUIRE(coordinates.z == 130);
-}
+    int val = uarmSwiftPro.charToInt(buffer, 0, 3);
 
-TEST_CASE("Coordinates 3D get coordinates negative") {
-    RobotArm::Coordinate3D coordinates(-140, -150, -130);
-
-    REQUIRE(coordinates.x == -140);
-    REQUIRE(coordinates.y == -150);
-    REQUIRE(coordinates.z == -130);
-}
-
-TEST_CASE("Coordinates 3D set coordinates positive") {
-    RobotArm::Coordinate3D coordinates;
-    coordinates.x = 140;
-    coordinates.y = 150;
-    coordinates.z = 130;
-
-    REQUIRE(coordinates.x == 140);
-    REQUIRE(coordinates.y == 150);
-    REQUIRE(coordinates.z == 130);
-}
-
-TEST_CASE("Coordinates 3D set coordinates negative") {
-    RobotArm::Coordinate3D coordinates;
-    coordinates.x = -140;
-    coordinates.y = -150;
-    coordinates.z = -130;
-
-    REQUIRE(coordinates.x == -140);
-    REQUIRE(coordinates.y == -150);
-    REQUIRE(coordinates.z == -130);
+    REQUIRE(val == 100);
 }
---
+++ test/test_move_queue.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file
+ * @brief     Tests for the move queue.
+ * @author    Wiebe van Breukelen
+ * @license   MIT
+ */
+
+#ifndef TEST_MOVE_QUEUE
+#define TEST_MOVE_QUEUE
+
+#include "catch.hpp"
+#include "../src/queue.hpp"
+#include "../src/coordinate3d.hpp"
+
+const int queueSize = 250;
+Queue<RobotArm::Coordinate3D, queueSize> queue;
+
+TEST_CASE("Clear queue") {
+    queue.push(RobotArm::Coordinate3D(10, 20, 30));
+    queue.push(RobotArm::Coordinate3D(10, 20, 30));
+
+    queue.clear();
+
+    REQUIRE(queue.count() == 0);
+}
+
+TEST_CASE("Add Coordinate3D object to queue") {
+    queue.clear();
+    queue.push(RobotArm::Coordinate3D(10, 20, 30));
+
+    REQUIRE(queue.count() == 1);
+}
+
+TEST_CASE("Add multiple Coordinate3D objects to queue") {
+    queue.clear();
+
+    for (int i = 0; i < queueSize + 10; i++) {
+        queue.push(RobotArm::Coordinate3D(10, 20, 30));
+    }
+
+    REQUIRE(queue.count() == 250);
+}
+
+TEST_CASE("Pop from queue") {
+    queue.clear();
+    queue.push(RobotArm::Coordinate3D(10, 20, 30));
+
+    REQUIRE((queue.pop() == RobotArm::Coordinate3D(10, 20, 30)));
+
+    REQUIRE(queue.count() == 0);
+}
+
+TEST_CASE("Peek from queue") {
+    queue.clear();
+    queue.push(RobotArm::Coordinate3D(10, 20, 30));
+
+    REQUIRE((queue.peek() == RobotArm::Coordinate3D(10, 20, 30)));
+    REQUIRE(queue.count() == 1);
+}
+
+#endif
